6c9e3c1ab8a8255a89001e5ba62a8b3f954fa179
Thomas Wanchaï MENIER <thomas.menier@etu.u-bordeaux.fr>
2022-04-21 09:40:30 +0200

    changed col2 to column flow

--- docs/js/project-display.js
+++ docs/js/project-display.js
@@ -5,7 +5,7 @@ function changeSubset() {
     const c1_val = document.querySelector('input[name="c1-items"]:checked').value
     if (c1_selected != c1_val) {
         for (let i=0; i<subsets.length; i++) {
-            subsets[i].style.right = c1_val * 100 + "%";
+            subsets[i].style.bottom = c1_val * 100 + "%";
         }
         c1_selected = c1_val;
     }
