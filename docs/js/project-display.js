92fd0c5836b920eff26d967ec8b516140052ee0b
Thomas Wanchaï MENIER <thomas.menier@etu.u-bordeaux.fr>
2022-04-28 10:56:13 +0200

    modified changeRetex() function, added task to complete for it to work

--- docs/js/project-display.js
+++ docs/js/project-display.js
@@ -1,5 +1,7 @@
 const subsets = document.getElementsByClassName('subset');
+const retexes = document.getElementsByClassName('retex-subset');
 var c1_selected = 0;
+var c2_selected = 0;
 
 function changeSubset_B() {
     const c1_val = document.querySelector('input[name="c1-items"]:checked').value
@@ -11,9 +13,14 @@ function changeSubset_B() {
     }
 }
 
-function changeRetex(colIndex) {
-    const c2Name  = "c2-items-" + colIndex;
+function changeRetex(colLetter) {
+    const c2Name  = "c2-items-" + colLetter;
     const c2Items = document.querySelector('input[name='+c2Name+']:checked');
     const c2_val  = c2Items.value;
-    
+    if (c2_selected != c2_val) {
+        for (let i=0; i<retexes.length; i++) {
+            //select retex of given column, then apply x*bottom to imgs
+        }
+        c2_selected = c2_val;
+    }
 }
